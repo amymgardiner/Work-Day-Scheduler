53772f6a426ad957a9bfef5aedd44070bb6947b3
Amy Gardiner <amymgardiner@gmail.com>
2022-06-12 17:47:25 -0500

    added pseudocode in js to figure out what functions to write

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,11 @@
 var date = moment().format('LLLL');
 $("#currentDay").html(date);
+
+// button click listener
+
+// save text in localstorage
+
+
+// get current time
+
+// change color of text area depending on time
