8f66cfbc96b8a9a91f3860838025a97f8f39413c
Amy Gardiner <amymgardiner@gmail.com>
2022-06-14 15:06:49 -0500

    started creating function to change colors on the work day scheduler's time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,41 @@
 $("#currentDay").text(moment().format('LLLL'));
 
-$(document).ready(function () {
-    $(".saveBtn").on("click", function () {
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
+function timeColor() {
+    //get current hour of the day
+    var currentTime = moment().hour();
 
-        localStorage.setItem(time, text);
+    //loop over time blocks
+    $(".time-block").each(function () {
+        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+        // check the time, and add the classes for background indicators depending on if the timeblock is in the past, present, or future
+        // for the past
+        if (blockTime < currentTime) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        // for the present
+        else if (blockTime === currentTime) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+        // for the future
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+
+        }
     })
+}
+
+$(".saveBtn").on("click", function () {
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, text);
 })
 
 $("#hour9 .description").val(localStorage.getItem("hour9"));
@@ -18,3 +47,5 @@ $("#hour14 .description").val(localStorage.getItem("hour14"));
 $("#hour15 .description").val(localStorage.getItem("hour15"));
 $("#hour16 .description").val(localStorage.getItem("hour16"));
 $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+timeTracker();
